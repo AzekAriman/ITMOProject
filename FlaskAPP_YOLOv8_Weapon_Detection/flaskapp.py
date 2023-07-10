3ef4afa4e6209c66da15c59fc0d39d72808f6283
AzekAriman <ilya300601@gmail.com>
2023-07-10 18:22:09 +0300

    1
    1

--- FlaskAPP_YOLOv8_Weapon_Detection/flaskapp.py
+++ FlaskAPP_YOLOv8_Weapon_Detection/flaskapp.py
@@ -19,7 +19,7 @@ from YOLO_Video import video_detection
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'muhammadmoin'
+app.config['SECRET_KEY'] = 'mhotep'
 app.config['UPLOAD_FOLDER'] = 'static/files'
 
 
