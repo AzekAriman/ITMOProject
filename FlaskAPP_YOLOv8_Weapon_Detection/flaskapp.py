525f7e8e5320217e74129129195b71c6f430fe59
AzekAriman <ilya300601@gmail.com>
2023-07-15 16:39:51 +0300

    Update flaskapp.py

--- FlaskAPP_YOLOv8_Weapon_Detection/flaskapp.py
+++ FlaskAPP_YOLOv8_Weapon_Detection/flaskapp.py
@@ -102,5 +102,5 @@ def webapp():
     return Response(generate_frames_web(path_x=0), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='127.0.0.1', port=5000)
 
